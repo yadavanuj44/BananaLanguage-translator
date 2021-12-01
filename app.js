ad889210385941fe8f34cf3c468079f26ee8c289
yadavanuj44 <95302917+yadavanuj44@users.noreply.github.com>
2021-12-02 00:31:42 +0530

    taking input in console

---
+++ app.js
@@ -0,0 +1,7 @@
+var Translatebtn = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#txt-input");
+
+Translatebtn.addEventListener("click", translateButtonHandler);
+function translateButtonHandler() {
+    console.log("inputText");
+};
